c55b2b554c9a4378112869c7ee53babeb6b369bf
Lucasgeg <99218923+Lucasgeg@users.noreply.github.com>
2022-06-14 09:09:28 +0200

    -env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    KEY_SENDGRID:
-      "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",
-  },
+  
 };
 
 module.exports = nextConfig;
